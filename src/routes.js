630dcfd2c5c8e3b8580d40f12aefe4cc0447380c
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-05-10 18:38:28 +0300

    Performance fixes

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,30 @@ var is = require('nor-is');
 var debug = require('nor-debug');
 var fs = require('nor-fs');
 
-/** Routes constructor */
-function Routes(opts, items) {
+/** Load targets */
+function load_targets(self) {
+	debug.assert(self).is('object');
+	debug.assert(self.items).is('array');
+	debug.assert(self.targets).is('object');
+
+	self.targets = {};
+	self.items.forEach(function(item) {
+		if( !(item && (item.path !== undefined) && (item.method !== undefined)) ) {
+			return;
+		}
+		if(self.targets[item.path] === undefined) {
+			self.targets[item.path] = {};
+		}
+		if(self.targets[item.path][item.method] !== undefined) {
+			debug.info("Detected duplicate: ", item.method, ' ', item.path, ' (ignored it)');
+			item.duplicate = true;
+			return;
+		}
+		self.targets[item.path][item.method] = item;
+	});
+}
+
+/** Routes constructor */ function Routes(opts, items) {
 	var self = this;
 	opts = opts || {};
 	items = items || [];
@@ -21,8 +43,14 @@ function Routes(opts, items) {
 	self.items = copy(items);
 	self.changed = opts.changed || false;
 
-	debug.assert(self.file).is('string');
 	debug.assert(self.items).is('array');
+
+	// Make an object (hash map) as an index for searching targets faster (path + method)
+	self.targets = {};
+	load_targets(self);
+
+	debug.assert(self.file).is('string');
+	debug.assert(self.targets).is('object');
 }
 
 /** Load routes from filesystem */
@@ -36,39 +64,21 @@ Routes.load = function(path) {
 	return new Routes({'file': path, 'changed':false}, items);
 };
 
+/** Returns true if target exists */
+Routes.prototype.targetExists = function target_exists(path, method) {
+	var self = this;
+	debug.assert(path).is('string');
+	debug.assert(method).is('string');
+	return !!( is.object(self.targets[path]) && is.object(self.targets[path][method]) );
+}
+
 /** Update the routes from Express App */
 Routes.prototype.update = function(app) {
 	var self = this;
 
 	debug.assert(self.items).is('array');
+	debug.assert(self.targets).is('object');
 	debug.assert(app).is('function');
-
-	var _paths = {};
-
-	self.items.forEach(function(route) {
-		debug.assert(route).is('object');
-		debug.assert(route.path).is('string');
-		debug.assert(route.method).is('string');
-
-		var path = route.path;
-
-		if(!is.object(_paths[path])) {
-			_paths[path] = {};
-		}
-
-		if(_paths[path][route.method] === undefined) {
-			_paths[path][route.method] = route;
-		} else {
-			debug.info("Detected duplicate in routes.json: ", route.method, ' ', route.path);
-			route.duplicate = true;
-		}
-	});
-
-	function _exists(path, method) {
-		debug.assert(path).is('string');
-		debug.assert(method).is('string');
-		return !!( is.object(_paths[path]) && is.object(_paths[path][method]) );
-	}
 
 	debug.assert(app.routes).is('object');
 
@@ -85,7 +95,7 @@ Routes.prototype.update = function(app) {
 			debug.assert(route.path).is('string');
 			debug.assert(route.method).is('string');
 
-			if(_exists(route.path, route.method)) {
+			if(self.targetExists(route.path, route.method)) {
 				return;
 			}
 
@@ -103,10 +113,10 @@ Routes.prototype.update = function(app) {
 			self.changed = true;
 			self.items.push(obj);
 
-			if(!_paths[route.path]) {
-				_paths[route.path] = {};
+			if(!self.targets[route.path]) {
+				self.targets[route.path] = {};
 			}
-			_paths[route.path][route.method] = obj;
+			self.targets[route.path][route.method] = obj;
 			debug.info("New route added to routes.json: ", route.method, ' ', route.path);
 		});
 	});
@@ -114,17 +124,55 @@ Routes.prototype.update = function(app) {
 	return self;
 };
 
-/** Find a route */
-Routes.prototype.find = function(opts) {
-	debug.assert(opts).is('object');
+/** Find a route by mixed options. Please note that this is much slower than specialized `_findTarget(path, method)`.
+ * @returns {array} All matching routes in an array
+ */
+Routes.prototype.findMixed = function(opts) {
 	var self = this;
+	debug.assert(opts).is('object');
 	debug.assert(self.items).is('array');
+	var opts_keys = Object.keys(opts);
 	return self.items.filter(function(route) {
 		debug.assert(route).is('object');
-		return Object.keys(opts).map(function(key) {
+		return opts_keys.map(function(key) {
 			return route[key] === opts[key] ? true : false;
 		}).every(is.true);
 	});
+};
+
+/** Find a route by path and method. 
+ * @returns {array} The routes which were found. Current implementation only returns one target because the cache contains only the first.
+ */
+Routes.prototype.findTarget = function(path, method) {
+	var self = this;
+	debug.assert(path).is('string');
+	debug.assert(method).is('string');
+	debug.assert(self.targets).is('object');
+	
+	var target = self.targets[path];
+	if(target === undefined) {
+		return [];
+	}
+
+	var route = target[method];
+	if(route === undefined) {
+		return [];
+	}
+	return [route];
+};
+
+/** Find routes by options 
+ * @returns {array} All matching routes in an array
+ */
+Routes.prototype.find = function(opts) {
+	debug.assert(opts).is('object');
+	var self = this;
+	var opts_keys = Object.keys(opts);
+	if( (opts_keys.length === 2) && is.defined(opts.path) && is.defined(opts.method) ) {
+		return self.findTarget(opts.path, opts.method);
+	} else {
+		return self.findMixed(opts);
+	}
 };
 
 /** Save the routes to file */
